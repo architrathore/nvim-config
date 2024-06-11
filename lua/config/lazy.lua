acf4d24c5adbf51aacc18795de97dab5bc4e6f49
Archit Rathore <architr@stripe.com>
2024-06-11 10:15:19 -0400

    feat: Add copilot-chat

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.coding.mini-surround" },
         { import = "lazyvim.plugins.extras.coding.copilot" },
+        { import = "lazyvim.plugins.extras.coding.copilot-chat" },
 
         -- import/override with your plugins
         { import = "plugins" },
