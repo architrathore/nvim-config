e17fa217856f5ff862601c0afeb588535d89a05f
Archit Rathore <architr@stripe.com>
2026-01-30 12:40:04 -0500

    chore: Pin ty and ruff

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,7 +2,8 @@ return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
         ensure_installed = {
-            "ty",
+            "ty@0.0.14",
+            "ruff@0.14.0",
         },
     },
 }
