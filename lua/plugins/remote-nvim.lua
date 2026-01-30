e5761c29604b91f441c9a8732e2e94802b135c44
Archit Rathore <architr@stripe.com>
2026-01-30 12:39:48 -0500

    exp: try out remote-nvim (didn't work)

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,25 @@
+return {
+    -- "amitds1997/remote-nvim.nvim",
+    -- dependencies = {
+    --     "nvim-lua/plenary.nvim", -- For standard functions
+    --     "MunifTanjim/nui.nvim", -- To build the plugin UI
+    --     -- "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    -- },
+    -- config = true,
+    -- opts = {
+    --     progress_view = {
+    --         type = "split",
+    --     },
+    --     offline_mode = {
+    --         enabled = true,
+    --         no_github = false,
+    --     },
+    --     remote = {
+    --         copy_dirs = {
+    --             compression = {
+    --                 enabled = true,
+    --             },
+    --         },
+    --     },
+    -- },
+}
