70ee8d8b4f5bb33c234ea2187b35e23a4d569038
Archit Rathore <architr@stripe.com>
2026-03-11 10:13:10 -0400

    chore: differentiate ssh on mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,16 @@
+local ensure_installed = {}
+
+-- Pin LSP versions on remote machines; locally, Mason installs latest by default.
+if os.getenv("SSH_TTY") then
+    ensure_installed = {
+        "ty@0.0.14",
+        "ruff@0.14.0",
+    }
+end
+
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {
-            "ty@0.0.14",
-            "ruff@0.14.0",
-        },
+        ensure_installed = ensure_installed,
     },
 }
