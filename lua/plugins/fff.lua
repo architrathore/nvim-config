33e4f3b6e96e8732b26c4f7aebbc139c11701270
Archit Rathore <architr@stripe.com>
2026-07-13 10:03:33 -0400

    chore: switch to fff

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,55 @@
+return {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+        -- downloads a prebuilt binary or falls back to cargo build
+        require("fff.download").download_or_build_binary()
+    end,
+    -- for nixos:
+    -- build = "nix run .#release",
+    opts = {
+        debug = {
+            enabled = false,
+            show_scores = true,
+        },
+        layout = {
+            preview_position = "right",
+            flex = {
+                size = 200,
+                wrap = "bottom",
+            },
+            prompt_position = "top",
+        },
+    },
+    lazy = false, -- the plugin lazy-initialises itself
+    keys = {
+        {
+            "<leader>ff",
+            function()
+                require("fff").find_files()
+            end,
+            desc = "FFFind files",
+        },
+        {
+            "<leader>fg",
+            function()
+                require("fff").live_grep()
+            end,
+            desc = "LiFFFe grep",
+        },
+        {
+            "<leader>fz",
+            function()
+                require("fff").live_grep({ grep = { modes = { "fuzzy", "plain" } } })
+            end,
+            desc = "Live fffuzy grep",
+        },
+        {
+            "<leader>fw",
+            function()
+                require("fff").live_grep_under_cursor()
+            end,
+            mode = { "n", "x" },
+            desc = "Search current word / selection",
+        },
+    },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,7 @@ return {
     opts = {
         ---@class snacks.picker.Config
         picker = {
+            enabled = false,
             matcher = {
                 frecency = true,
             },
